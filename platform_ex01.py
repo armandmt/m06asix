4ec5ece62fae24ec595ac4d4dd53787d8c34f1df
Armand Mata <amata3@xtec.cat>
2021-03-14 00:47:41 +0100

    1

---
+++ platform_ex01.py
@@ -0,0 +1,14 @@
+import platform
+
+print(platform.platform())
+print("Processador: " + platform.processor())
+print("Màquina: " + platform.machine())
+print("Sistema: " + platform.system())
+print("Versió SO: " + platform.version())
+print("Release (Versió kernel): " + platform.release())
+print("Node (Nom de xarxa) : " + platform.node())
+
+maquina = platform.uname()
+print("Tot: maquina  ")
+print(maquina)
+print("Indexant amb [] obtenim els valors individuals: " + maquina[0])
