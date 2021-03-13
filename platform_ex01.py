cfd3e9fbbd38768ba584074d15d803a4630869f6
Armand <amata3@xtec.cat>
2021-03-14 00:59:50 +0100

    2

--- platform_ex01.py
+++ platform_ex01.py
@@ -7,10 +7,11 @@ print("Sistema: " + platform.system())
 print("Versió SO: " + platform.version())
 print("Release (Versió kernel): " + platform.release())
 print("Node (Nom de xarxa) : " + platform.node())
+
+print ("Això és un MAC? ")
+print (platform.mac_ver() )
+
 maquina = platform.uname()
 print("Tot: maquina  ")
 print(maquina)
 print("Indexant amb [] obtenim els valors individuals: " + maquina[0])
-
-
-print("Sistema: " + platform.system())
